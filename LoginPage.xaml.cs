3a88e4e6c6d52cfb2d7b73dcddfedde8ceb9aa7a
Maur Cordeiro <167926814+EuMaur@users.noreply.github.com>
2025-10-29 08:18:16 -0300

    Add login logic to LoginPage
    Implement login functionality with user validation.

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -1,1 +1,30 @@
+using Laprass;
 
+
+
+namespace Laprass;
+
+public partial class LoginPage : ContentPage
+{
+    public LoginPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        string usuario = UserEntry.Text;
+        string senha = PasswordEntry.Text;
+
+        
+        if (usuario == "admin" && senha == "1234")
+        {
+            await Navigation.PushAsync(new ProdutosPage());
+        }
+        else
+        {
+            ErrorLabel.Text = "Usuário ou senha incorretos!";
+            ErrorLabel.IsVisible = true;
+        }
+    }
+}
