ce8fc92ccaf4a8974c68a0443da5852f5c40f8a1
Maur Cordeiro <167926814+EuMaur@users.noreply.github.com>
2025-10-29 08:17:13 -0300

    Add ProdutosPage with product list initialization

--- ProdutosPage.xaml.cs
+++ ProdutosPage.xaml.cs
@@ -1,1 +1,27 @@
+namespace Laprass;
+
+public partial class ProdutosPage : ContentPage
+{
+    public ProdutosPage()
+    {
+        InitializeComponent();
+
+        var produtos = new List<Produto>
+        {
+            new Produto { Nome = "Maçã", Preco = "R$ 2,00 / kg", Imagem = "maca.png" },
+            new Produto { Nome = "Banana", Preco = "R$ 8,00 / dúzia", Imagem = "banana.png" },
+            new Produto { Nome = "Melancia", Preco = "R$ 7,00 / unidade", Imagem = "melancia.png" },
+            new Produto { Nome = "Uva", Preco = "R$ 4,00 / cacho", Imagem = "uva.png" }
+        };
+
+        ProdutosCollection.ItemsSource = produtos;
+    }
+}
+
+public class Produto
+{
+    public string Nome { get; set; }
+    public string Preco { get; set; }
+    public string Imagem { get; set; }
+}
 
